739edfbffdfb60f1df2350a0b3943eafc3ab82fa
lohit244 <lohit244@gmail.com>
2023-03-30 09:18:30 +0530

    +1 on last commit

--- out/constants.js
+++ out/constants.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.apiBaseUrl = void 0;
-exports.apiBaseUrl = "https://vsbuddiesextension.herokuapp.com";
+exports.apiBaseUrl = "https://vsbuddiesextapi-production.up.railway.app";
 //# sourceMappingURL=constants.js.map
